33bcbc4d531f7d1c5d8b6f34ebdb787849c479b0
Fambegbe Olamileke <olamileke.f@gmail.com>
2021-02-13 13:28:48 +0100

    ui fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -89,7 +89,7 @@
                 <img src='/images/home/hero.jpg' class='object-cover absolute top-0 left-0 w-full h-full'>
             </div>
         </div>
-        <div class='sm:pt-24 px-6 sm:px-16 md:px-24 lg:px-64'>
+        <div class='sm:pt-20 px-6 sm:px-16 md:px-24 lg:px-64'>
             <div class='flex flex-col-reverse bmd:grid bmd:grid-cols-12 col-gap-5 px-5 sm:px-10 py-10 bmd:p-16 bg-white sm:shadow-md'>
                 <div class='w-full bmd:col-span-6 flex flex-col justify-center lowercase bmd:pr-5'>
                     <p class='m-0 mb-4 bmd:mb-5 text-lg text-center bmd:text-left font-semibold'>What is Yeet and what can it do for you</p>
@@ -104,7 +104,7 @@
                 </div>
             </div>
         </div>
-        t<div class='grid grid-cols-12 px-6 sm:px-16 md:px-24 bmd:px-6 py-20 sm:mt-16 bmd:my-24 bg-yeet-blue text-white'>
+        t<div class='grid grid-cols-12 px-6 sm:px-16 md:px-24 bmd:px-6 py-20 sm:mt-16 bmd:mt-16 bg-yeet-blue text-white'>
             <div class='h-full hidden bmd:flex col-start-2 col-end-6 flex-col justify-center'>
                 <p class='m-0 mb-3 text-xl'>YeetBot</p>
                 <p class='m-0 mb-5 leading-7 text-justify'>Yeet is equiped with a powerful Telegram bot named YeetBot through which users receive price notifications. An essential component of
@@ -124,31 +124,31 @@
                 <a href='{{ route("signup") }}' class='py-4 px-6 text-white text-md shadow-md mr-4' style='width:fit-content'>YeetBot <i class='ml-2 text-sm fa fa-external-link-alt'></i></a>
             </div>
         </div>
-        <div class='py-8 sm:py-12 md:py-20 px-8 sm:px-12 md:px-20 lg:pl-32 lg:pr-24 bg-light-gray'>
+        <div class='py-8 sm:py-12 md:py-24 px-8 sm:px-12 md:px-20 lg:pl-28 lg:pr-24 bg-light-gray'>
             <div class='w-full bmd:px-8 flex flex-col lg:flex-row'>
-                <div class='w-full mb-10 lg:mb-0 lg:w-1/2 lg:pr-10'>
+                <div class='w-full mb-10 lg:mb-0 lg:w-1/2 lg:ml-5 lg:pr-10'>
                     <img src='/images/home/bitcoin.jpg' class='object-cover setup-image'>
                 </div>
                 <div class='w-full text-justify sm:text-left lg:w-1/2 lg:pl-10 flex flex-col'>
                     <div class='mb-4'>
-                        <p class='m-0'>Getting started with yeet is very easy. Easy like counting 1, 2, 3, 4. Follow the steps below
+                        <p class='m-0 text-justify'>Getting started with yeet is very easy. Easy like counting 1, 2, 3, 4. Follow the steps below
                         and in no time you will be enjoying the convenience Yeet provides.</p>
                     </div>
                     <div class='flex flex-col mb-4'>
-                        <p class='m-0 mb-2 bmd:mb-1 text-center sm:text-left text-md font-semibold'>1. Create Your Yeet Account</p>
-                        <p class='m-0'>Well, as you are here already, create your Yeet account. Or if you do have an account already (you are so smart), login to Yeet.</p>
+                        <p class='m-0 mb-2 bmd:mb-1 text-md font-semibold'>1. Create Your Yeet Account</p>
+                        <p class='m-0 text-justify'>Well, as you are here already, create your Yeet account. Or if you do have an account already (you are so smart), login to Yeet.</p>
                     </div>
                     <div class='flex flex-col mb-4'>
-                        <p class='m-0 mb-2 bmd:mb-1 text-center sm:text-left text-md font-semibold'>2. Add Subscriptions</p>
-                        <p class='m-0'>From your stocks or cryptocurrencies tabs in Yeet, find stocks you have in your portfolio or simply want to monitor and subscribe to them.</p>
+                        <p class='m-0 mb-2 bmd:mb-1 text-md font-semibold'>2. Add Subscriptions</p>
+                        <p class='m-0 text-justify'>From your stocks or cryptocurrencies tabs in Yeet, find stocks or crypto you have in your portfolio or simply want to monitor and subscribe to them.</p>
                     </div>
                     <div class='flex flex-col mb-4'>
-                        <p class='m-0 mb-2 bmd:mb-1 text-center sm:text-left text-md font-semibold'>3. Register with YeetBot</p>
-                        <p class='m-0'>Visit <a class='text-blue-800' href='#'>t.me/yeetbot</a> or open up your Telegram app and search for YeetBot. Follow its prompts to register your Telegram contact with Yeet.</p>
+                        <p class='m-0 mb-2 bmd:mb-1 text-md font-semibold'>3. Register with YeetBot</p>
+                        <p class='m-0 text-justify'>Visit <a class='text-blue-800' href='#'>t.me/yeetbot</a> or open up your Telegram app and search for YeetBot. Follow its prompts to register your Telegram contact with Yeet.</p>
                     </div>
                     <div class='flex flex-col'>
-                        <p class='m-0 mb-2 bmd:mb-1 text-center sm:text-left text-md font-semibold'>4. Receive your Notifications</p>
-                        <p class='m-0'>Every night at 8:00pm, YeetBot via Telegram will send you the market prices of all the stocks and crytocurrencies you are subscribed to.</p>
+                        <p class='m-0 mb-2 bmd:mb-1 text-md font-semibold'>4. Receive your Notifications</p>
+                        <p class='m-0 text-justify'>Every night at 8:00pm, YeetBot via Telegram will send you the market prices of all the stocks and crytocurrencies you are subscribed to.</p>
                     </div>
                 </div>
             </div>
@@ -158,7 +158,7 @@
                 <p class='m-0 mb-2 text-xl'>Yeet</p>
                 <div>Providing ease in share price tracking and monitoring by leveraging core technologies. <p class='m-0 hidden bmd:inline'>Stay updated with your favorite stocks and cryptocurrencies.</p></div>
             </div>
-            <div class='col-span-12 mb-4 bsm:mb-0 bsm:col-start-5 bsm:col-span-5 flex flex-col bsm:pl-12 lg:pl-24'>
+            <div class='col-span-12 mb-4 bsm:mb-0 bsm:col-start-5 bsm:col-span-5 flex flex-col bsm:pl-12 lg:pl-32'>
                 <p class='m-0 mb-2 bsm:mb-3 text-xl'>Contact</p>
                 <p class='m-0 mb-2 bsm:mb-3'>
                     <i class='fas fa-map-marker-alt mr-3'></i>
@@ -173,8 +173,8 @@
                     <a href='mailto:olamileke.dev@gmail.com'>olamileke.dev@gmail.com</a>
                 </p>
             </div>
-            <div class='col-span-12 bsm:col-start-10 bsm:col-span-3 lg:pl-12 bsm:ml-3 flex flex-col'>
-                <p class='m-0 mb-2 bsm:mb-3 text-xl'>Socials</p>
+            <div class='col-span-12 bsm:col-start-10 bsm:col-span-3 lg:pl-12 bsm:ml-8 flex flex-col'>
+                <p class='m-0 mb-2 bsm:mb-3 text-xl'>Social Media</p>
                 <p class='m-0 mb-2 bsm:mb-3'>
                     <i class='fab fa-facebook mr-3'></i>
                     <a href='http://facebook.com' target="blank">Facebook</a>
