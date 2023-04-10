ee683e41a20a1c4085a4a33e54f2c71c9b62443c
Fambegbe Olamileke <olamileke.f@gmail.com>
2023-04-10 15:54:49 +0100

    fix: fixed issue with resource url ssl issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
 
         Relation::morphMap([
             'stock' => 'App\Models\Stock',
